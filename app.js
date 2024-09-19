392861094087e3c82a18491200c4b871c528f960
SatyapriyaMahato <satyapriyamahto@gmail.com>
2024-09-19 14:11:58 +0530

    add projects section

--- app.js
+++ app.js
@@ -1,12 +1,42 @@
-// const menuBtn = document.querySelector(".menu-button");
-// menuBtn.addEventListener("click", function () {
-//     document.querySelector(".site-logo").classList.toggle("alt-logo");
-//     document.querySelector(".menu-social-icons").classList.toggle("hidden");
-//     document.body.style.position = "fixed";
-
-// })
+let sections = document.querySelectorAll('.div-project-tile');
 
 function toggleMenu() {
     const menu = document.querySelector('.menu');
     menu.classList.toggle('active');
 }
+
+let lastScrollY = window.scrollY;
+
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('.header');
+    if (window.scrollY > lastScrollY) {
+        // User is scrolling down, hide the header
+        header.style.top = '-60px'; // Adjust based on header height
+    } else {
+        // User is scrolling up, show the header
+        header.style.top = '0';
+    }
+    lastScrollY = window.scrollY;
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const tiles = document.querySelectorAll('.div-project-tile');
+
+    const observerOptions = {
+        threshold: 0.1 // When 10% of the tile is visible
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target); // Stop observing once visible
+            }
+        });
+    }, observerOptions);
+
+    tiles.forEach(tile => {
+        observer.observe(tile);
+    });
+});
